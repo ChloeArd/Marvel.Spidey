c75d3f29eefe8f07bde2af4b1c3f0405ec0e54a6
ChloeArd <chloe.ardoise@gmail.com>
2021-09-08 16:31:50 +0200

    Responsive du site

--- View/movies.php
+++ View/movies.php
@@ -24,7 +24,7 @@
 
     <h2 class="titleChara">SPIDER-MAN : TOM HOLLAND</h2>
     <div id="containerMovies" class="width_80 flexRow wrap auto">
-        <a href="" class="width_px center">
+        <a href="movie.php" class="width_px center">
             <img class="imgMovies" src="https://fr.web.img3.acsta.net/c_310_420/pictures/16/03/11/09/46/182814.jpg">
             <p class="titleMovies">Captain America : Civil War</p>
         </a>
